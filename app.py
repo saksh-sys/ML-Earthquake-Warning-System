755960a128ccf4239221f692232a3044e8186879
saksh-sys <66247423+saksh-sys@users.noreply.github.com>
2024-05-02 19:14:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 from joblib import load
 from sklearn.preprocessing import StandardScaler
-import folium
-from streamlit_folium import folium_static
 
 # Load your trained model and scaler
 model = load('XG_Boost_model.joblib')
@@ -28,22 +26,37 @@ year = st.number_input('Year', min_value=2000, max_value=2023, value=2021)
 day_of_year = st.number_input('Day of the Year', min_value=1, max_value=365, value=1)
 hour = st.number_input('Hour of the Day', min_value=0, max_value=23, value=12)
 
-# Create a Folium map
-mymap = folium.Map(location=[latitude, longitude], zoom_start=6)
-
-# Display the map in Streamlit app
-folium_static(mymap)
-
-# Define a function to handle the click event
-def handle_click(latitude, longitude):
-    # Display the coordinates in Streamlit
-    st.write(f'Clicked Latitude: {latitude}, Clicked Longitude: {longitude}')
-
-# Add a click event handler to the map using Streamlit's st.map function
-st.map(mymap, on_click=handle_click)
-
 # Button to make prediction
 if st.button('Predict Magnitude'):
     features = [latitude, longitude, depth, year, day_of_year, hour]
     prediction = predict_earthquake(features)
     st.write(f'Predicted Magnitude: {prediction[0]}')
+
+
+st.header("*Contributors*")
+        contributors = {
+            "ANSH VARSHNEY": {"email": "anshvarshney3@gmail.com", 
+                              "github_link": "https://github.com/ansh0707",
+                              "details":{
+                                  "Reg No" : "12006893",
+                                  "University":"Lovely Professional University"
+                              }},
+            "SARA BORA": {"email": "sarabora420@gmail.com", 
+                          "github_link": "https://github.com/sara-bora",
+                          "details":{
+                              "Reg No" : "12013194",
+                              "University":"Lovely Professional University"
+                          }},
+            "SARTHAK MISHRA": {"email": "sam4sarthak@gmail.com",
+                                "github_link": "https://github.com/SarthakMishra0307",
+                                "details":{
+                                    "Reg No" : "12018433",
+                                    "University":"Lovely Professional University"
+                                }},
+            "SATYAM DUBEY": {"email": "satyamdubey2988@gmail.com",
+                             "github_link": "https://github.com/dubeysatyam2002",
+                             "details":{
+                                    "Reg No" : "12014267",
+                                    "University":"Lovely Professional University"
+                            }},
+        }
