1532fc32db5aca4ca59bb36e9bcefff2351ad8dc
saksh-sys <66247423+saksh-sys@users.noreply.github.com>
2024-05-02 19:11:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ st.title('Earthquake Magnitude Prediction')
 st.write('This application predicts the magnitude of earthquakes based on input features.')
 
 # Input fields
-#latitude = st.number_input('Latitude', value=0.0)
-#longitude = st.number_input('Longitude', value=0.0)
+latitude = st.number_input('Latitude', value=0.0)
+longitude = st.number_input('Longitude', value=0.0)
 depth = st.number_input('Depth', value=0.0)
 year = st.number_input('Year', min_value=2000, max_value=2023, value=2021)
 day_of_year = st.number_input('Day of the Year', min_value=1, max_value=365, value=1)
@@ -35,9 +35,9 @@ mymap = folium.Map(location=[latitude, longitude], zoom_start=6)
 folium_static(mymap)
 
 # Define a function to handle the click event
-def handle_click(lat, lon):
+def handle_click(latitude, longitude):
     # Display the coordinates in Streamlit
-    st.write(f'Clicked Latitude: {lat}, Clicked Longitude: {lon}')
+    st.write(f'Clicked Latitude: {latitude}, Clicked Longitude: {longitude}')
 
 # Add a click event handler to the map using Streamlit's st.map function
 st.map(mymap, on_click=handle_click)
