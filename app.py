9e62a50f95cbe394afa6df10292c86bb5aa6387a
saksh-sys <66247423+saksh-sys@users.noreply.github.com>
2024-05-02 19:38:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 from joblib import load
 from sklearn.preprocessing import StandardScaler
-import folium  # Import Folium for map visualization
 
 # Load your trained model and scaler
 model = load('XG_Boost_model.joblib')
@@ -19,20 +18,7 @@ def predict_earthquake(features):
 st.title('Earthquake Magnitude Prediction')
 st.write('This application predicts the magnitude of earthquakes based on input features.')
 
-# Create a map for location selection
-st.header('Select Location on Map')
-map_center = [0, 0]  # Initial map center
-map_zoom = 2  # Initial zoom level
-
-# Display map
-m = folium.Map([38.8934, -76.9470], tiles='stamentoner', zoom_start=12)
-st.markdown(m.repr_html(), unsafe_allow_html =True)
-
-#my_map = folium.Map(location=map_center, zoom_start=map_zoom)
-folium.Marker(location=map_center, popup='Selected Location').add_to(m)
-#st.markdown(my_map._repr_html_(), unsafe_allow_html=True)
-
-# Get location from user
+# Input fields
 latitude = st.number_input('Latitude', value=0.0)
 longitude = st.number_input('Longitude', value=0.0)
 depth = st.number_input('Depth', value=0.0)
@@ -40,8 +26,13 @@ year = st.number_input('Year', min_value=2000, max_value=2023, value=2021)
 day_of_year = st.number_input('Day of the Year', min_value=1, max_value=365, value=1)
 hour = st.number_input('Hour of the Day', min_value=0, max_value=23, value=12)
 
+# Map for selecting location
+selected_location = st.map()
+latitude_from_map = selected_location.latlng[0] if selected_location else 0.0
+longitude_from_map = selected_location.latlng[1] if selected_location else 0.0
+
 # Button to make prediction
 if st.button('Predict Magnitude'):
-    features = [latitude, longitude, depth, year, day_of_year, hour]
+    features = [latitude_from_map, longitude_from_map, depth, year, day_of_year, hour]
     prediction = predict_earthquake(features)
     st.write(f'Predicted Magnitude: {prediction[0]}')
