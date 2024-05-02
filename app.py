7a145ffb930fd91ca59819531054fc22f8519069
saksh-sys <66247423+saksh-sys@users.noreply.github.com>
2024-05-02 19:40:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 from joblib import load
 from sklearn.preprocessing import StandardScaler
+import folium
+from streamlit_folium import folium_static
 
 # Load your trained model and scaler
 model = load('XG_Boost_model.joblib')
@@ -26,13 +28,17 @@ year = st.number_input('Year', min_value=2000, max_value=2023, value=2021)
 day_of_year = st.number_input('Day of the Year', min_value=1, max_value=365, value=1)
 hour = st.number_input('Hour of the Day', min_value=0, max_value=23, value=12)
 
-# Map for selecting location
-selected_location = st.map()
-latitude_from_map = selected_location.latlng[0] if selected_location else 0.0
-longitude_from_map = selected_location.latlng[1] if selected_location else 0.0
+# Create a Folium map
+m = folium.Map(location=[latitude, longitude], zoom_start=6)
+
+# Add marker for selected location
+folium.Marker([latitude, longitude], popup='Selected Location').add_to(m)
+
+# Display the map in Streamlit app
+folium_static(m)
 
 # Button to make prediction
 if st.button('Predict Magnitude'):
-    features = [latitude_from_map, longitude_from_map, depth, year, day_of_year, hour]
+    features = [latitude, longitude, depth, year, day_of_year, hour]
     prediction = predict_earthquake(features)
     st.write(f'Predicted Magnitude: {prediction[0]}')
